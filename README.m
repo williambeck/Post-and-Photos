1efa19839fc44e89bf347711d3c5138ca40ed1af
william beck <willbeckcg@gmail.com>
2021-07-27 22:05:12 -0300

    first commit

---
+++ README.m
@@ -0,0 +1,1 @@
+# POSTS-AND-PHOTOS
---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,17 @@
+import '../Posts/styles.css' 
+ 
+import { PostCard } from '../PostCard';
+
+export const Posts = ({ posts }) => (
+  <div className="posts">
+    {posts.map(post => (
+      <PostCard
+        key={post.id}
+        title={post.title}
+        body={post.body}
+        id={post.id}
+        cover={post.cover}
+      />
+    ))}
+  </div>
+);
